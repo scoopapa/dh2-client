0bf743e374fc376baaf5b476e3e47b291be78cac
Rezzo64 <34076225+Rezzo64@users.noreply.github.com>
2023-03-20 17:58:28 +0000

    fix login server path

--- js/client.js
+++ js/client.js
@@ -218,7 +218,7 @@ function toId() {
 		getActionPHP: function () {
 			var ret = '/~~' + Config.server.id + '/action.php';
 			if (Config.testclient) {
-				ret = 'https://' + Config.routes.client + ret;
+				ret = 'https://play.pokemonshowdown.com/action.php';
 			}
 			return (this.getActionPHP = function () {
 				return ret;
