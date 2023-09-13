ca21bfc74f2c00cfc5f4e67c0b7f18182e1380e0
Sean <34076225+Rezzo64@users.noreply.github.com>
2023-09-13 18:47:24 +0100

    fix useful/useless moves bug

--- src/battle-dex-search.ts
+++ src/battle-dex-search.ts
@@ -1599,7 +1599,7 @@ class BattleMoveSearch extends BattleTypedSearch<'move'> {
 			return true;
 		}
 		// Custom move added by a mod
-		if (this.mod && id in BattleTeambuilderTable[this.mod].overrideMoveInfo && !BattleTeambuilderTable[this.mod].overrideMoveInfo[id].unviable) return true;
+		if (this.mod && id in BattleTeambuilderTable[this.mod].overrideMoveInfo && BattleTeambuilderTable[this.mod].overrideMoveInfo[id].customUseful) return true;
 		const modMoveData = BattleMovedex[id];
 		if (!modMoveData) return true;
 		if (modMoveData.category === 'Status') {
