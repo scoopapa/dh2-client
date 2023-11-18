9267500f686035a70e6cc98dbae83e541b80c696
Rezzo <rezzo64@gmail.com>
2023-11-18 23:44:47 +0000

    add Substitute as placeholder sprite

--- src/battle-animations.ts
+++ src/battle-animations.ts
@@ -825,8 +825,11 @@ export class BattleScene implements BattleSceneStub {
 				if (textBuf) textBuf += ' / ';
 				textBuf += pokemon.speciesForme;
 				let url = spriteData.url;
+				var placeholderSprite = spriteData.isFrontSprite // Pet Mods placeholder sprites
+				? "https://play.pokemonshowdown.com/sprites/gen5/substitute.png"
+				: "https://play.pokemonshowdown.com/sprites/gen5-back/substitute.png";
 				// if (this.paused) url.replace('/xyani', '/xy').replace('.gif', '.png');
-				buf += '<img src="' + url + '" width="' + spriteData.w + '" height="' + spriteData.h + '" style="position:absolute;top:' + Math.floor(y - spriteData.h / 2) + 'px;left:' + Math.floor(x - spriteData.w / 2) + 'px" />';
+				buf += '<img src="' + url + '" width="' + spriteData.w + '" height="' + spriteData.h + '" style="position:absolute;top:' + Math.floor(y - spriteData.h / 2) + 'px;left:' + Math.floor(x - spriteData.w / 2) + 'px" onerror="this.src=\'' + placeholderSprite + '\'"/>';
 				buf2 += '<div style="position:absolute;top:' + (y + 45) + 'px;left:' + (x - 40) + 'px;width:80px;font-size:10px;text-align:center;color:#FFF;">';
 				const gender = pokemon.gender;
 				if (gender === 'M' || gender === 'F') {
