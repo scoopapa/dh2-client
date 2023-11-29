c5b94a5a886a36f031fa6e21018bf92ee74c1a55
Sean <34076225+Rezzo64@users.noreply.github.com>
2023-11-29 23:06:53 +0000

    Add web URL to config.js

--- config/config.js
+++ config/config.js
@@ -36,7 +36,7 @@ Config.customcolors = {
 Config.version = "0.11.2";
 
 Config.routes = {
-	root: '191.101.232.116',
+	root: 'petmodsdh.com',
 	client: 'localhost',
 	dex: 'dex.pokemonshowdown.com',
 	replays: 'replay.pokemonshowdown.com',
