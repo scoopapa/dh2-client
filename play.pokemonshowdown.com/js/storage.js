57c3e5ecf899e50f8cc39c303011872f5713a203
Rezzo <rezzo64@gmail.com>
2023-12-29 22:17:47 +0000

    update hp type check for validation, fr this time

--- play.pokemonshowdown.com/js/storage.js
+++ play.pokemonshowdown.com/js/storage.js
@@ -1368,11 +1368,11 @@ Storage.importTeam = function (buffer, teams) {
 			if (line.substr(0, 14) === 'Hidden Power [') {
 				var hptype = line.substr(14, line.length - 15);
 				line = 'Hidden Power ' + hptype;
-				var type = Dex.types.get(hptype.toLowerCase());
-				if (!curSet.ivs && window.BattleTypeChart && window.BattleTypeChart[type]) {
+				var type = Dex.types.get(hptype);
+				if (!curSet.ivs && type) {
 					curSet.ivs = {};
-					for (var stat in window.BattleTypeChart[type].HPivs) {
-						curSet.ivs[stat] = window.BattleTypeChart[type].HPivs[stat];
+					for (var stat in type.HPivs) {
+						curSet.ivs[stat] = type.HPivs[stat];
 					}
 				}
 			}
