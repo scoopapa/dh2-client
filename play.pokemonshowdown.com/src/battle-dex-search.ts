e73374678f10d6fe116f69c30a8489d3501cdd18
scoopapa <pccompositor@aim.com>
2024-01-09 09:22:51 -0600

    Update battle-dex-search.ts
    changing this back because we discussed why this shouldn't have been changed in the first place

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -1661,8 +1661,8 @@ class BattleMoveSearch extends BattleTypedSearch<'move'> {
 		}
 		const moveData = dex.moves.get(id);
 		if (!moveData) return true;
+		if (!BattleMovedex[id].exists) return true; //Flag ALL custom moves as viable by default (unviable custom moves almost never happen, no need to add extra steps to create multi-hit moves)
 		if (moveData.category === 'Status') {
-			if (!BattleMovedex[id].exists) return true; //Flag custom moves as viable by default
 			return BattleMoveSearch.GOOD_STATUS_MOVES.includes(id);
 		}
 		if (moveData.flags?.charge) {
