0536f20be9879df5444949e50e6179da69516215
rezzo64 <rezzo64@github.com>
2025-12-13 13:08:40 +0000

    add fixes to build, e.g. filepaths and toID function

--- config/config.js
+++ config/config.js
@@ -36,11 +36,12 @@ Config.customcolors = {
 Config.version = "0.11.2";
 
 Config.routes = {
-	root: '191.101.232.116',
-	client: 'localhost',
+	root: 'api.petmodsdh.com',
+	client: 'petmodsdh.com',
 	dex: 'dex.pokemonshowdown.com',
 	replays: 'replay.pokemonshowdown.com',
 	users: 'pokemonshowdown.com/users',
+	teams: 'teams.pokemonshowdown.com',
 	psmain: 'pokemonshowdown.com',
 };
 /*** End automatically generated configuration ***/
